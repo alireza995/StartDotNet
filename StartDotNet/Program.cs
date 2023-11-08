0848e7a14c85fe12a38989cf4ad47ad55752df91
Alireza <ma.sayedbagheri@gmail.com>
2023-11-09 01:16:11 +0330

    Create a sample project to simplify start of programming with .NET

---
+++ StartDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using StartDotNet;
+
+Console.WriteLine("Project started for first time");
+var stop = false;
+while (!stop)
+{
+    Console.WriteLine(YourCode.WriteHere());
+    Console.WriteLine("Press any key to restart project or ESC to exit.");
+    var key = Console.ReadKey().Key;
+    stop = key == ConsoleKey.Escape;
+}
+Console.WriteLine("Project stopped");
---
+++ StartDotNet/YourCode.cs
@@ -0,0 +1,35 @@
+namespace StartDotNet;
+
+public class YourCode
+{
+    public static string WriteHere()
+    {
+        var result = "";
+        
+        /////////////////////شروع کد/////////////////////////
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ////////////////////////پایان کد./////////////////////
+
+        return result;
+    }
+}
